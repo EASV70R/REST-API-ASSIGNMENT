f11498f51ebe9986d9a84124ea21c1dfdc65e613
EASV70R <75667950+EASV70R@users.noreply.github.com>
2023-01-30 16:52:44 +0100

    basic CRUD template done

--- routes/test.js
+++ routes/test.js
@@ -3,15 +3,6 @@ const router = require('express').Router();
 // Import the test model
 let test = require('../models/testschema');
 
-// Read all tests
-router.route('/').get((req, res) => {
-    test.find()
-        .then(data => res.json(data))
-        .catch(err => {
-            res.status(500).send({ message: err.message })
-        });
-});
-
 // Create a test
 router.post('/', (req, res) => {
     const name = req.body.name;
@@ -29,5 +20,61 @@ router.post('/', (req, res) => {
         });
 });
 
+// Read all tests
+router.get('/', (req, res) => {
+    test.find()
+        .then(data => { res.send(data); })
+        .catch(err => {
+            res.status(500).send({ message: err.message })
+        });
+});
+
+// Read a test by id
+router.get('/:id', (req, res) => {
+    test.findById(req.params.id)
+        .then(data => {
+            if (!data) {
+                res.status(500).send({ message: "Test not found with id " + req.params.id });
+            } else {
+                res.send(data);
+            }
+        })
+        .catch(err => {
+            res.status(500).send({ message: err.message })
+        });
+});
+
+// Update a test by id
+router.put('/:id', (req, res) => {
+    test.findByIdAndUpdate(req.params.id, {
+            name: req.body.name,
+            age: req.body.age,
+        }, { new: true })
+        .then(data => {
+            if (!data) {
+                res.status(500).send({ message: "Test not found with id " + req.params.id });
+            } else {
+                res.send({ message: "Test updated successfully." });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({ message: err.message })
+        });
+});
+
+// Delete a test by id
+router.delete('/:id', (req, res) => {
+    test.findByIdAndDelete(req.params.id)
+        .then(data => {
+            if (!data) {
+                res.status(500).send({ message: "Test not found with id " + req.params.id });
+            } else {
+                res.send({ message: "Test deleted successfully." });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({ message: err.message })
+        });
+});
 
 module.exports = router;
